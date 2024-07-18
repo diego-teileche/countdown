737069dee8aaae96fb0591fb091bf3791a4a3756
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-18 17:27:11 -0300

    CountDown Project Finished
    - Logic created
    - Styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css"
+import Timer from "./timer/Timer"
 
 function App() {
 	return (
-		<>
-			<h1>Diego!</h1>
-		</>
+		<div className="App">
+			<Timer duration={2 * 24 * 60 * 60 * 1000} />
+		</div>
 	)
 }
 
